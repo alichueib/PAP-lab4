2cec3df38ba6d095f56c5dc0c07f34bb7948fe1c
Ali Chueib <alichueib@gmail.com>
2026-04-18 16:38:04 +0200

    Exercise 1 - Done and tested

--- exercise_1.c
+++ exercise_1.c
@@ -26,27 +26,32 @@
 void lbm_comm_init_ex1(lbm_comm_t * comm, int total_width, int total_height)
 {
 	int rank;
-	int comm_size;
+	int size;
 	int local_width_without_ghost;
 
+	// Get rank information from the global communicator.
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	comm->nb_x = comm_size;
+	// 1D decomposition: all ranks are arranged along X only.
+	comm->nb_x = size;
 	comm->nb_y = 1;
 
 	comm->rank_x = rank;
 	comm->rank_y = 0;
 
-	if (total_width % comm->nb_x != 0)
+	// The global width must be divisible by the number of X sub-domains.
+	if (total_width % size != 0)
 		MPI_Abort(MPI_COMM_WORLD, -1);
 
-	local_width_without_ghost = total_width / comm->nb_x;
+	local_width_without_ghost = total_width / size;
 
+	// Add one ghost column on each side and one ghost row on top/bottom.
 	comm->width = local_width_without_ghost + 2;
 	comm->height = total_height + 2;
 
-	comm->x = comm->rank_x * local_width_without_ghost;
+	// Absolute position in the global mesh, excluding ghosts.
+	comm->x = rank * local_width_without_ghost;
 	comm->y = 0;
 
 	//if debug print comm
@@ -56,16 +61,44 @@ void lbm_comm_init_ex1(lbm_comm_t * comm, int total_width, int total_height)
 /****************************************************/
 void lbm_comm_ghost_exchange_ex1(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
-	//
-	// TODO: Implement the 1D communication with blocking MPI functions (MPI_Send & MPI_Recv)
-	//
-	// To be used:
-	//    - DIRECTIONS: the number of doubles composing a cell
-	//    - double[DIRECTIONS] lbm_mesh_get_cell(mesh, x, y): function to get the address of a particular cell.
-	//    - comm->width : The with of the local sub-domain (containing the ghost cells)
-	//    - comm->height : The height of the local sub-domain (containing the ghost cells)
-	
-	//example to access cell
-	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
-	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	int left_rank;
+	int right_rank;
+	const int column_size = comm->height * DIRECTIONS;
+
+	left_rank = (comm->rank_x == 0) ? MPI_PROC_NULL : comm->rank_x - 1;
+	right_rank = (comm->rank_x == comm->nb_x - 1) ? MPI_PROC_NULL : comm->rank_x + 1;
+
+	// Send our first interior column (x = 1) to the left neighbor and
+	// receive the right neighbor interior column into our right ghost (x = width - 1).
+	MPI_Sendrecv(
+		lbm_mesh_get_cell(mesh, 1, 0),
+		column_size,
+		MPI_DOUBLE,
+		left_rank,
+		0,
+		lbm_mesh_get_cell(mesh, comm->width - 1, 0),
+		column_size,
+		MPI_DOUBLE,
+		right_rank,
+		0,
+		MPI_COMM_WORLD,
+		MPI_STATUS_IGNORE
+	);
+
+	// Send our last interior column (x = width - 2) to the right neighbor and
+	// receive the left neighbor interior column into our left ghost (x = 0).
+	MPI_Sendrecv(
+		lbm_mesh_get_cell(mesh, comm->width - 2, 0),
+		column_size,
+		MPI_DOUBLE,
+		right_rank,
+		1,
+		lbm_mesh_get_cell(mesh, 0, 0),
+		column_size,
+		MPI_DOUBLE,
+		left_rank,
+		1,
+		MPI_COMM_WORLD,
+		MPI_STATUS_IGNORE
+	);
 }
