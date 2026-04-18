1b18c25243252e0cb0412cf8675dad9b3014990c
Ali Chueib <alichueib@gmail.com>
2026-04-18 16:52:23 +0200

    Exercise 3 Done -Tested

--- exercise_3.c
+++ exercise_3.c
@@ -32,16 +32,58 @@ void lbm_comm_init_ex3(lbm_comm_t * comm, int total_width, int total_height)
 /****************************************************/
 void lbm_comm_ghost_exchange_ex3(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
-	//
-	// TODO: Implement the 1D communication with non-blocking MPI functions.
-	//
-	// To be used:
-	//    - DIRECTIONS: the number of doubles composing a cell
-	//    - double[9] lbm_mesh_get_cell(mesh, x, y): function to get the address of a particular cell.
-	//    - comm->width : The with of the local sub-domain (containing the ghost cells)
-	//    - comm->height : The height of the local sub-domain (containing the ghost cells)
-	
-	//example to access cell
-	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
-	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	int left_rank;
+	int right_rank;
+	int req_count = 0;
+	MPI_Request requests[4];
+	const int column_size = comm->height * DIRECTIONS;
+
+	left_rank = (comm->rank_x == 0) ? MPI_PROC_NULL : comm->rank_x - 1;
+	right_rank = (comm->rank_x == comm->nb_x - 1) ? MPI_PROC_NULL : comm->rank_x + 1;
+
+	// Receive the left neighbor interior column into our left ghost column (x = 0).
+	MPI_Irecv(
+		lbm_mesh_get_cell(mesh, 0, 0),
+		column_size,
+		MPI_DOUBLE,
+		left_rank,
+		1,
+		MPI_COMM_WORLD,
+		&requests[req_count++]
+	);
+
+	// Receive the right neighbor interior column into our right ghost column (x = width - 1).
+	MPI_Irecv(
+		lbm_mesh_get_cell(mesh, comm->width - 1, 0),
+		column_size,
+		MPI_DOUBLE,
+		right_rank,
+		0,
+		MPI_COMM_WORLD,
+		&requests[req_count++]
+	);
+
+	// Send our first interior column (x = 1) to the left neighbor.
+	MPI_Isend(
+		lbm_mesh_get_cell(mesh, 1, 0),
+		column_size,
+		MPI_DOUBLE,
+		left_rank,
+		0,
+		MPI_COMM_WORLD,
+		&requests[req_count++]
+	);
+
+	// Send our last interior column (x = width - 2) to the right neighbor.
+	MPI_Isend(
+		lbm_mesh_get_cell(mesh, comm->width - 2, 0),
+		column_size,
+		MPI_DOUBLE,
+		right_rank,
+		1,
+		MPI_COMM_WORLD,
+		&requests[req_count++]
+	);
+
+	MPI_Waitall(req_count, requests, MPI_STATUSES_IGNORE);
 }
